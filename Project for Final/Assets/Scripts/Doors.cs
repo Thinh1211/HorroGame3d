4374f597782d37b13c0aba47ae21c236c11199f1
Thinhld <laiducthinh1211@gmail.com>
2024-07-21 10:03:57 +0700

    new scene

--- Project for Final/Assets/Scripts/Doors.cs
+++ Project for Final/Assets/Scripts/Doors.cs
@@ -33,7 +33,7 @@ public class Doors : MonoBehaviour
         }
     }
 
-
+    
     void Update()
     {
 
---
+++ Project for Final/Assets/Scripts/RoomMaze.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMaze : MonoBehaviour
+{
+    public List<GameObject> Room;
+    void Start()
+    {
+        StartCoroutine(DeactivateRandomObjectsOverTime(0.5f, 3));
+    }
+
+     void Update()
+    {
+    }
+
+    IEnumerator DeactivateRandomObjectsOverTime(float duration, int numberOfObjects)
+    {
+        yield return new WaitForSeconds(duration);
+
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            if (Room.Count > 0)
+            {
+                int randomIndex = Random.Range(0,Room.Count);
+                Room[randomIndex].SetActive(false);
+                Room.RemoveAt(randomIndex);
+            }
+        }
+    }
+}
